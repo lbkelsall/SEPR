a04646197b39996d788acbc3898fd7c3e77afae6
Chris Whitlam <cw1416@york.ac.uk>
2016-11-22 12:59:49 +0000

    Basic Character Classes
    Basic Character Classes

---
+++ Homicide in the Hub/Assets/Classes/Character.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+abstract public class Character {
+	private string characterID;
+	private Sprite sprite;
+
+	public Character(){
+	}
+
+	public string getCharacterID(){
+		return this.characterID;
+	}
+}
---
+++ Homicide in the Hub/Assets/Classes/PlayerCharacter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacter : Character {
+	private Vector2 locationInScene;
+	private string currentScene;
+	public PlayerCharacter(Vector2 _locationInScene){
+		locationInScene = _locationInScene;
+	}
+
+}
