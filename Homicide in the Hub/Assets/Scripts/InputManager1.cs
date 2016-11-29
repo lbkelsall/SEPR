48493b3eb713fb7c8686093159eecd310f85ecef
tacticalsloth07 <jajh502@york.ac.uk>
2016-11-29 12:17:32 +0000

    Map now works
    Map input now works

--- Homicide in the Hub/Assets/Scripts/InputManager1.cs
+++ Homicide in the Hub/Assets/Scripts/InputManager1.cs
@@ -6,19 +6,31 @@ public class InputManager1 : MonoBehaviour {
 	public bool isMapvisible = false;
 	public bool isMenuvisible = false;
 
+	public GameObject map;
+
 	void Start () {
 
+
+
 	}
-	
+
 
 	void Update () {
 	
 		if(Input.GetKeyDown(KeyCode.M)){
-			isMapvisible = !isMapvisible
+			isMapvisible = !isMapvisible;
+		}
+
+		if (isMapvisible == true) {
+			map.SetActive (true);
+		} else {
+			map.SetActive (false);
+
 		}
 
 		if(Input.GetKeyDown(KeyCode.Escape)){
-			isMenuvisible != isMenuvisible
+			isMenuvisible = !isMenuvisible;
 		}
 
+}
 }
