cdca34e015d958343a69edf2a0ed6289e289e8d6
Chris Whitlam <cw1416@york.ac.uk>
2016-11-29 18:43:46 +0000

    Got Map Working and Pause Menu
    Stopped bug of pause menu and map appearing at the same time. Need to
    fix bug of player being unable to move when re-entering from the main
    menu

--- Homicide in the Hub/Assets/Scripts/InputManager1.cs
+++ Homicide in the Hub/Assets/Scripts/InputManager1.cs
@@ -7,30 +7,49 @@ public class InputManager1 : MonoBehaviour {
 	public bool isMenuvisible = false;
 
 	public GameObject map;
-
+	public GameObject pauseMenu; 
+	public GameObject detective; 
+	private PlayerMovement playerMovement;
 	void Start () {
-
-
-
+		playerMovement = detective.GetComponent<PlayerMovement>();
 	}
 
 
 	void Update () {
-	
-		if(Input.GetKeyDown(KeyCode.M)){
-			isMapvisible = !isMapvisible;
+		if (!isMenuvisible) {
+			if (Input.GetKeyDown (KeyCode.M)) {
+				isMapvisible = !isMapvisible;
+				if (isMapvisible) {
+					StopGame (map);
+				} else {
+					ResumeGame (map);
+				}
+			}
 		}
 
-		if (isMapvisible == true) {
-			map.SetActive (true);
-		} else {
-			map.SetActive (false);
+		if (!isMapvisible) {	
+			if (Input.GetKeyDown (KeyCode.Escape)) {
+				isMenuvisible = !isMenuvisible;
+				if (isMenuvisible) {
+					StopGame (pauseMenu);
+				} else {
+					ResumeGame (pauseMenu);
+				}
+			}
 
 		}
 
-		if(Input.GetKeyDown(KeyCode.Escape)){
-			isMenuvisible = !isMenuvisible;
-		}
+	}
 
-}
+	void StopGame(GameObject menu){
+		Time.timeScale = 0; 
+		playerMovement.enabled = false;
+		menu.SetActive (true);
+	}
+		
+	public void ResumeGame(GameObject menu){
+		Time.timeScale = 1; 
+		playerMovement.enabled = true;
+		menu.SetActive (false);
+	}
 }
---
+++ Homicide in the Hub/Assets/Scripts/TraverseRooms.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TraverseRooms : MonoBehaviour {
+
+	public string level;
+	// Use this for initialization
+	void OnMouseDown() {
+		SceneManager.LoadScene(level);
+		Debug.Log ("Load Level:" + level);
+	}
+}
