5b35741d13b232b6a39d6577e51bb20346647aba
tacticalsloth07 <jajh502@york.ac.uk>
2016-11-29 11:54:16 +0000

    Added Input manager script
    Input manager added

---
+++ Homicide in the Hub/Assets/Scripts/InputManager1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager1 : MonoBehaviour {
+
+	public bool isMapvisible = false;
+	public bool isMenuvisible = false;
+
+	void Start () {
+
+	}
+	
+
+	void Update () {
+	
+		if(Input.GetKeyDown(KeyCode.M)){
+			isMapvisible = !isMapvisible
+		}
+
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			isMenuvisible != isMenuvisible
+		}
+
+}
