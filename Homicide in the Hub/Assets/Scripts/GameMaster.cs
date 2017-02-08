010981d741a0341eea85deee83447469a8d102cb
LAPTOP-TD9GOLAD\simon <simondvsn@gmail.com>
2017-02-08 11:49:33 +0000

    Fixing some scripting errors in merge

--- Homicide in the Hub/Assets/Scripts/GameMaster.cs
+++ Homicide in the Hub/Assets/Scripts/GameMaster.cs
@@ -91,6 +91,9 @@ using System.Linq; //Used for take in pick items
 	private List<Item> relevant_items;
 	private List<VerbalClue> relevant_verbal_clues;
 
+	//WEDUNNIT
+	private float gameScore;
+
 	//Sets as a Singleton
 	void Awake () {  //Makes this a singleton class on awake
 		if (instance == null) { //Does an instance already exist?
@@ -100,7 +103,15 @@ using System.Linq; //Used for take in pick items
 		}
 		DontDestroyOnLoad (gameObject); //Set this to not be destroyed when reloading scene
 	}
-
+		
+	//WEDUNNIT
+	void Update(){
+		if (gameScore > 0) {
+			gameScore -= Time.deltaTime;
+		} else {
+			gameScore = 0;
+		}
+	}
 
 	void Start(){
 		//Initialises Variables
@@ -339,7 +350,21 @@ using System.Linq; //Used for take in pick items
 		AssignNPCsToScenes (characters,scenes);				//Assigns NPCS to scenes
 		AssignItemsToScenes (itemClues,scenes);					//Assigns Items to scenes
 		playerCharacter = detective;
-	}	
+
+		//WEDUNNIT
+		gameScore = 1000;
+	}
+
+	//WEDUNNIT
+	public void Penalise (float penalty){
+		gameScore -= penalty;
+	}
+
+	//WEDUNNIT
+	public int GetScore (){
+		int intScore = (int)gameScore;
+		return intScore;
+	}
 		
 
 	public PlayerCharacter GetPlayerCharacter(){
